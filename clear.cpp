30e36e36c3a0b660ae54f5ae484b46657827bcce
Helios <35069323+wlhcode@users.noreply.github.com>
2019-03-10 02:12:28 +0800

    Add files via upload

---
+++ clear.cpp
@@ -0,0 +1,26 @@
+#include<windows.h>
+#include<iostream>
+#include<cstdio>
+#include<fstream>
+using namespace std;
+int main(){
+	cout<<"Clear your blog. Are you sure? (y/n) ";
+	char c;
+	cin>>c;
+	if(c!='Y'&&c!='y') return 0;
+	 
+	string command = "del /Q ";
+	string path = "html\\*.html";
+	system(command.append(path).c_str());
+	
+	ifstream copy("html/temp_index_SPARE.txt");
+	ofstream index("index.html");
+	ofstream help("html/temp_index.txt");
+	
+	string line;
+	while(getline(copy,line)){
+		index<<line<<endl;
+		help<<line<<endl;
+	}
+	cout<<"index.html has been reset."<<endl;
+}
